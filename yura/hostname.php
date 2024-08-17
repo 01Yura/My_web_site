6b56b02ae3e3fb143d33f96b8020feef0d0a521c
Yuri Primyshev <p020885@yandex.ru>
2024-08-16 21:06:25 -0400

    3

--- yura/hostname.php
+++ yura/hostname.php
@@ -1,3 +1,3 @@
 <?php$hostname = gethostname();
-echo"<h1>Имя данного хоста: <span style='font-size: 24px;'>$HOSTNAME</span></h1>";
+echo "<h1>Имя данного хоста: <span style='font-size: 24px;'>$HOSTNAME</span></h1>";
 ?>
